c5e37d958d79e953c7268a17bc74b900b4b40b12
Mikeysoar <mwclimb@gmail.com>
2021-06-16 20:24:04 -0600

    my code looks like theirs

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const fs = require('fs');
+nodeconst fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
 const profileDataArgs = process.argv.slice(2);
 
+console.log(profileDataArgs);
 
 const [name, github] = profileDataArgs;
 
+console.log(name, github);
 
+const pageHTML = generatePage(name, github);
 
 fs.writeFile('./index.html', generatePage(name, github), err => {
   if (err) throw new Error(err);
