d5d5efc6bada8b27c8c613658cf53572ace8946c
Mikeysoar <mwclimb@gmail.com>
2021-06-19 14:55:15 -0600

    whoops

--- src/page-template.js
+++ src/page-template.js
@@ -17,4 +17,24 @@ const generatePage = (name, github) => {
     `;
   };
 
-  module.exports = generatePage;
+  module.exports = templateData => {
+    console.log(templateData);
+  
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+  
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${templateData.name}</h1>
+      <h2><a href="https://github.com/${templateData.github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+  };
